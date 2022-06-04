983ac5fbf09299f5390a714123c4d2b8829247c1
Shinobou <86394469+Shinobou@users.noreply.github.com>
2022-06-04 15:39:29 -0500

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,85 @@
-use captcha_rs::{Captcha, CaptchaBuilder};
+use captcha_rs::{CaptchaBuilder};
+
 use pyo3::prelude::*;
-use pyo3::types::PyBytes;
-use base64::decode;
+
 
 #[pyclass(name="Captcha")]
 struct PyCaptcha {
-    pub captcha: Captcha
+    pub text: String,
+    pub base_img: String,
+    pub dark_mode: bool,
+}
+
+#[pyclass(name="CaptchaBuilder")]
+struct PyCaptchaBuilder {
+    pub length: Option<usize>,
+    pub width: Option<u32>,
+    pub height: Option<u32>,
+    pub dark_mode: Option<bool>,
+    pub complexity: Option<u32>,
 }
 
 #[pymethods]
-impl PyCaptcha {
+impl PyCaptchaBuilder {
     #[new]
-    fn new(length: usize, width: u32, height: u32, dark_mode: bool, complexity: u32) -> Self {
-        PyCaptcha {
-            captcha: CaptchaBuilder::new()
-                .length(length)
-                .width(width)
-                .height(height)
-                .dark_mode(dark_mode)
-                .complexity(complexity)
-                .build()
+    pub fn new() -> Self {
+        Self {
+            length: None,
+            width: None,
+            height: None,
+            dark_mode: None,
+            complexity: None
         }
     }
 
-    fn get_text(&self) -> PyResult<String> {
-        Ok((&self.captcha.text).to_string())
+    pub fn length(mut slf: PyRefMut<Self>, length: usize) -> PyRefMut<Self> {
+        slf.length = Some(length);
+        slf
     }
 
-    fn get_bytes(&self, py: Python) -> PyResult<PyObject> {
-        let decoded: Vec<u8> = decode(self.captcha.base_img.split(",").collect::<Vec<&str>>().last().unwrap()).unwrap();
-        Ok(PyBytes::new(py, &*decoded).into())
+    pub fn width(mut slf: PyRefMut<Self>, width: u32) -> PyRefMut<Self> {
+        slf.width = Some(width);
+        slf
+    }
+
+    pub fn height(mut slf: PyRefMut<Self>, height: u32) -> PyRefMut<Self> {
+        slf.height = Some(height);
+        slf
+    }
+
+    pub fn dark_mode(mut slf: PyRefMut<Self>, dark_mode: bool) -> PyRefMut<Self> {
+        slf.dark_mode = Some(dark_mode);
+        slf
+    }
+
+    pub fn complexity(mut slf: PyRefMut<Self>, complexity: u32) -> PyRefMut<Self> {
+        let mut complexity = complexity;
+        if complexity > 10 { complexity = 10; }
+        if complexity < 1 { complexity = 1; }
+        slf.complexity = Some(complexity);
+        slf
+    }
+
+    pub fn build(&self) -> PyCaptcha {
+        let builder = CaptchaBuilder::new()
+            .length(self.length.unwrap())
+            .width(self.width.unwrap())
+            .height(self.height.unwrap())
+            .dark_mode(self.dark_mode.unwrap())
+            .complexity(self.complexity.unwrap());
+        let captcha = builder.build();
+        PyCaptcha {
+            text: captcha.text,
+            base_img: captcha.base_img,
+            dark_mode: captcha.dark_mode
+        }
     }
 }
-
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn captcha(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyCaptcha>()?;
+    m.add_class::<PyCaptchaBuilder>()?;
     Ok(())
 }
